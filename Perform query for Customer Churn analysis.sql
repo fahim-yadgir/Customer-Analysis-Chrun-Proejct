6823fec8a635ef5ca14739cb664cd5ce51fefcdc
Fahim <fahimyadgir07@gmail.com>
2026-06-17 21:38:23 +0530

    perform some basic query in SQL

--- Perform query for Customer Churn analysis.sql
+++ Perform query for Customer Churn analysis.sql
@@ -1,3 +1,27 @@
+select count(*)as total_customer
+from customer_churn;
+
+select * 
+from customer_churn
+where churn = 'Yes';
+
+select churn , count(*)as total_count
+from customer_churn
+group by churn;
+
+select round(avg(MonthlyCharges),2) as avg_MonthlyCharges
+from customer_churn;
+
+select max(tenure) as max_tenure , min(tenure)as min_tenure
+from customer_churn;
+
+select * from customer_churn
+where tenure >= 24;
+
+select gender , count(*) as total_count
+from customer_churn
+group by gender;
+
 select customerID , churn,count(*)as total_count
 from customer_churn
 group by customerID,churn
@@ -22,5 +46,5 @@ order by max_churn desc
 limit 5;
 
 select round(
-				count(case when churn = 'Yes' then 1 end )*100.0/count(*),2)as total_churn_percent
+count(case when churn = 'Yes' then 1 end)*100.0/count(*),2)as percentage_churn
 from customer_churn;
